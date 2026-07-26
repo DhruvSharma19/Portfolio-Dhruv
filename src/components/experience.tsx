6f5728954e47bf12b7b276f09698b26b64cc1d4a
Invinsible <112254552+DhruvSharma19@users.noreply.github.com>
2026-07-26 14:16:15 +0530

    Update experience.tsx

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -23,9 +23,7 @@ const Experience = ({ timeline }: ExperienceProps) => {
       <span className="blob absolute top-[20%] left-0 w-1/3 h-5/6 blur-[100px] -z-10" />
 
       <SectionHeading className="pl-4 md:px-12 py-20">
-        <SlideIn className="text-white/40">Experience</SlideIn>
-        <br />
-        <SlideIn>Skills</SlideIn>
+        <SlideIn>Experience</SlideIn>
       </SectionHeading>
 
       <div>
